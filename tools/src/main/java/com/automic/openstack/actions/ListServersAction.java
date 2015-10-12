8543f4b9ff3e9e6443e0a3dc261dba9e6d567d18
Anurag Upadhyay <anurag.upadhyay@nagarro.com>
2015-10-12 13:52:45 +0530

    PCK_OS-10<PCK_OS-34>: List Server json to xml parsing code commit

--- tools/src/main/java/com/automic/openstack/actions/ListServersAction.java
+++ tools/src/main/java/com/automic/openstack/actions/ListServersAction.java
@@ -115,9 +115,9 @@ public class ListServersAction extends AbstractAction{
 
     private void prepareOutput(ClientResponse response) throws AutomicException {
 
-        JSONObject jsonObj = CommonUtil.jsonResponse(response.getEntityInputStream());
-        CommonUtil.json2xml(jsonObj, filePath);      
-       
+       JSONObject jsonObj = CommonUtil.jsonResponse(response.getEntityInputStream());
+        jsonObj = jsonObj.put("Server", jsonObj.get("servers"));
+        CommonUtil.json2xml(jsonObj , filePath, "Servers");
         ConsoleWriter.writeln("UC4RB_OPS_LIST_SERVERS_XML ::=" + filePath);
 
     }
