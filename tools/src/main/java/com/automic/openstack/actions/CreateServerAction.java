5655a7f302b0e3f9610ce754e8ce5c83dea1e467
Anurag Upadhyay <anurag.upadhyay@nagarro.com>
2015-10-19 17:31:12 +0530

    PCK_OS-15<PCK_OS-85>: Create server JAVA new proposed chnages initial code commit

--- tools/src/main/java/com/automic/openstack/actions/CreateServerAction.java
+++ tools/src/main/java/com/automic/openstack/actions/CreateServerAction.java
@@ -86,7 +86,7 @@ public class CreateServerAction extends AbstractHttpAction{
 
         ClientResponse response = null;
 
-        //tokenId = AESEncryptDecrypt.decrypt(tokenId);
+        tokenId = AESEncryptDecrypt.decrypt(tokenId);
 
         WebResource webResource = client.resource(baseUrl).path(tenantId).path("servers");
 
